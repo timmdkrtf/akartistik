b19b2a56e1885a3b2c9c91fa1d714ded8a02f8f2
timmdkrtf <contact@mdkrtf.co.id>
2025-03-26 09:22:51 +0700

    revisi website

--- src/components/AboutPT.jsx
+++ src/components/AboutPT.jsx
@@ -9,7 +9,7 @@ export default function AboutPT(){
                     <div className="text">
                         <p>PT Abdi Karya Artistik didirikan pada tahun 2024. Kami adalah perusahaan yang bergerak di bidang <span className="fst-italic">fashion</span> muslimah dan merupakan anak perusahaan dari Thamrin Meyer Corporation.</p>
                         <p>Dengan visi menghadirkan busana yang sesuai dengan nilai-nilai Islam, kami berkomitmen untuk menciptakan pakaian yang tidak hanya anggun dan nyaman, tetapi juga sesuai syariah.</p>
-                        <p>Melalui brand Kisahkisa, kami hadir sebagai <span className="fst-italic">brand fashion</span> muslimah yang modern, elegan, dan tetap mengutamakan kesantunan. Dengan desain yang indah dan material berkualitas, Kisahkisa hadir untuk mendukung muslimah dalam berbusana sesuai syariah tanpa mengesampingkan gaya dan kenyamanan.</p>
+                        <p>Melalui <span className="fst-italic">brand</span> Kisahkisa, kami hadir sebagai <span className="fst-italic">brand fashion</span> muslimah yang modern, elegan, dan tetap mengutamakan kesantunan. Dengan desain yang indah dan material berkualitas, Kisahkisa hadir untuk mendukung muslimah dalam berbusana sesuai syariah tanpa mengesampingkan gaya dan kenyamanan.</p>
                     </div>
                 </div>
             </div>
